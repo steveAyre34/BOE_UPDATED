3c7a34ed39534bdc94278055e974ad145b79a805
steveAyre34 <stephen.ayre@aol.com>
2017-06-13 10:21:17 -0400

    Finished Householded Exporter
    I will continue to optimize exporter, but it now works.

--- query_boe.php
+++ query_boe.php
@@ -22,6 +22,7 @@
 	$query = str_replace("count(voter_id) as this_count", $columns_selected, $query);
 	$query = str_replace("count(DISTINCT last_name, street_no, street_name, apt_no) as this_count", $columns_selected, $query);
 	$sql_count = "SELECT ";
+	$array_counts = array();
 	if(isset($_POST["household"])){
 		$query .= " GROUP BY last_name, street_no, street_name, apt_no";
 		$result = mysqli_query($conn, $query);
@@ -32,18 +33,26 @@
 			$street_name = $row["street_name"];
 			$apt_no = $row["apt_no"];
 			$sql_count .= "sum(last_name = '$last_name' AND street_no = '$street_no' AND street_name = '$street_name' AND apt_no = '$apt_no') as count" . $count . ", ";
+			if(fmod($count, 1000) == 0){
+				$sql_count = rtrim($sql_count,", ");
+				$sql_count .= " FROM $table_name";
+				array_push($array_counts, $sql_count);
+				$sql_count = "SELECT ";
+			}
 			$count++;
 		}
 		$sql_count = rtrim($sql_count,", ");
 		$sql_count .= " FROM $table_name";
+		array_push($array_counts, $sql_count);
 		//echo $sql_count;
 	}
 	$result = mysqli_query($conn, $query);
 	fputcsv($file, $headers);
 	if(isset($_POST["household"])){
-		$result_counts = mysqli_query($conn, $sql_count);
+		$result_counts = mysqli_query($conn, $array_counts[0]);
 		$row_counts = $result_counts->fetch_assoc();
 		$count = 1;
+		$array_index = 1;
 		while($row = $result->fetch_assoc()){
 			if($row_counts["count" . $count] > 1){
 				$row["last_name"] = "The " . $row["last_name"] . " Family";
@@ -54,6 +63,12 @@
 			else{
 				fputcsv($file, $row);
 			}
+			if(fmod($count, 1000) == 0){
+				$result_counts = mysqli_query($conn, $array_counts[$array_index]);
+				$row_counts = $result_counts->fetch_assoc();
+				$array_index++;
+			}
+			$count++;
 		}
 	}
 	else{
